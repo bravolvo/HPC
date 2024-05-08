a4bad2e33ae3f894d7cdb81520b5100e4babfe32
Clovis Wabila Vudinga <84052557+bravolvo@users.noreply.github.com>
2024-05-09 00:23:52 +0400

    Add files via upload

---
+++ Install_docker.sh
@@ -0,0 +1,10 @@
+# Install docker on compute nodes
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+sudo groupadd docker                                
+sudo usermod -aG docker ec2-user
+
+# Install Singularity
+sudo yum install -y singularity-ce
