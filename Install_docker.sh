b63d16c6f07254e3b7dab68646c60842b370e0c0
Clovis Wabila Vudinga <84052557+bravolvo@users.noreply.github.com>
2024-05-09 00:51:38 +0400

    Update Install_docker.sh

--- Install_docker.sh
+++ Install_docker.sh
@@ -1,10 +1,13 @@
 # Install docker on compute nodes
+sudo yum install -y epel-release
+sudo yum update -y
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl enable --now docker
 sudo groupadd docker                                
 sudo usermod -aG docker ec2-user
+exec sudo su -l $USER
 
 # Install Singularity
 sudo yum install -y singularity-ce
